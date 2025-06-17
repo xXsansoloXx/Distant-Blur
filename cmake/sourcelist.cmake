fad9a2f10fd43bc2df17cb19906aee8dbb92759b
Little Pooh <77505794+xXsansoloXx@users.noreply.github.com>
2025-06-17 14:57:14 -0500

    Initial Commit

---
+++ cmake/sourcelist.cmake
@@ -0,0 +1,10 @@
+set(sources ${sources}
+	src/Events.cpp
+	src/plugin.cpp
+	src/Utils.cpp
+	src/Settings.cpp
+	src/Manager.cpp
+	src/Hooks.cpp
+	src/MCP.cpp
+ 	src/Serialization.cpp
+)
---
+++ src/plugin.cpp
@@ -0,0 +1,20 @@
+#include "logger.h"
+
+void OnMessage(SKSE::MessagingInterface::Message* message) {
+    if (message->type == SKSE::MessagingInterface::kDataLoaded) {
+        // Start
+    }
+    /* if (message->type == SKSE::MessagingInterface::kNewGame ||
+             message->type == SKSE::MessagingInterface::kPostLoadGame) {
+        // Post-load
+    }*/
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface *skse) {
+
+    SetupLog();
+    logger::info("Plugin loaded");
+    SKSE::Init(skse);
+    SKSE::GetMessagingInterface()->RegisterListener(OnMessage);
+    return true;
+}
