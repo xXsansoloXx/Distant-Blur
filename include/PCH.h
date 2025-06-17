cbd46338f750b48a4655c844f69aa697e75fa7a5
Little Pooh <77505794+xXsansoloXx@users.noreply.github.com>
2025-06-17 14:57:14 -0500

    Initial Commit

---
+++ include/PCH.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "RE/Skyrim.h"
+#include "SKSE/SKSE.h"
+#include <spdlog/sinks/basic_file_sink.h>
+#include <wrl/client.h>
+
+#include <cstdint>            // For std::uint8_t, std::uint32_t, std::uint64_t etc.
+#include <string>
+#include <vector>
+
+namespace logger = SKSE::log;
+using namespace std::literals;
